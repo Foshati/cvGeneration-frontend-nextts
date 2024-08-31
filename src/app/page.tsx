c82427b3072bd1e36b89ac9e79b5f90076d9aed8
Foshati <amirrezafoshati@gmail.com>
2024-08-31 23:56:31 +0330

    create saveImage function for donload img

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import DisplayCv from "@/components/displayCv";
 import FormCv from "@/components/formCv";
 import React, { useState } from "react";
+import { toPng } from "html-to-image";
 
 interface CvData {
   fullName: string;
@@ -24,6 +25,17 @@ const dataObj: CvData = {
 
 export default function Home() {
   const [data, setData] = useState<CvData>(dataObj);
+
+  const displayRef = React.useRef<HTMLDivElement>(null);
+  const saveImage = async () => {
+    const url = await toPng(displayRef.current, { pixelRatio: 1.5 });
+    const fileName = "efi.png";
+
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = fileName;
+    a.click();
+  };
 
   return (
     <main className="flex flex-col justify-center items-center bg-[#0C0D0E] text-white h-screen gap-4 p-4">
